006a15af5efc3482210b92622387dcd09477bab2
jvlazar <jvlazar@gmail.com>
2023-05-03 22:24:00 -0600

    color working with input from first row

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-let value;
+let word;
+
+let rowNumber = 1;
 
 
 async function fetchWord(){
@@ -6,8 +8,8 @@ async function fetchWord(){
   
     try {
         const response = await fetch(url);
-        const word = await response.json();
-        return word;
+        const value = await response.json();
+        return value;
     }
    catch (error) {
     console.error(error);
@@ -16,21 +18,57 @@ async function fetchWord(){
 }
   
 
-function checkInput(){
-    console.log(value);
+function getInput(rownum){
+    let userInput= "";
+    for (let i = 1; i < 6; i++){
+        let letter = document.getElementById('field'+i).value;
+        userInput += letter;
+    }
+    checkInput(userInput);
+    console.log(`looking at ${userInput}`);
 }
 
-function changeColour(){
-    document.getElementById("field1").style.backgroundColor = "green";
+function checkInput(str){
+   const array = [];
+
+    // get the number of times each letter appears
+    for (let i = 0; i < word.length; i++){
+        array[i]= word.split(word[i]).length - 1;
+    }
+
+    for (let i = 0; i < array.length; i++){
+        console.log(`the counts for letter ${word[i]} is ${array[i]}`)
+    }
+
+   // get all the green values
+    for (let i = 0; i < str.length; i++){
+        if (str[i] == word[i]){
+            console.log(`the letters are the same in position ${i+1}`)
+            changeColour("green", `field`+(i+1));
+        }
+        else if (word.includes(str[i]) && (word.split(word[i]).length - 1) >= 1){
+            // get all the yellow values
+            changeColour("yellow", `field`+(i+1));
+        } else {
+            
+        }
+    }
+
+
+}
+
+
+function changeColour(colour, id){
+    document.getElementById(id).style.backgroundColor = colour;
 }
 
 
 async function main(){
-    const word = await fetchWord();
-    value = word[0];
+    const value = await fetchWord();
+    word = "hello";
 
 
-    console.log(`the word is ${value}`);
+    console.log(`the word is ${word}`);
     
 }
 
