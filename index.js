9a7c9d3f96b2113d5755fd97fc48386745c6c065
jvlazar <jvlazar@gmail.com>
2023-05-03 21:23:12 -0600

    got 5 letter word from API call

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
+let value;
 
 
-function getWord(){
+async function fetchWord(){
+    let url = 'https://random-word-api.vercel.app/api?words=1&length=5';
+  
+    try {
+        const response = await fetch(url);
+        const word = await response.json();
+        return word;
+    }
+   catch (error) {
+    console.error(error);
+   }
 
 }
+  
 
 function checkInput(){
- 
+    console.log(value);
 }
 
 function changeColour(){
     document.getElementById("field1").style.backgroundColor = "green";
 }
+
+
+async function main(){
+    const word = await fetchWord();
+    value = word[0];
+
+
+    console.log(`the word is ${value}`);
+    
+}
+
+
+
+
+
+main();
+
+
+
+
+
