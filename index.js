17e1ad632cbadde04e454981dc3f42fcbc4c1e1c
jvlazar <jvlazar@gmail.com>
2023-05-04 19:51:02 -0600

    enabling fields per row works, still need to fix color changes per row

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let word;
 let rowNumber = 1;
+let maxRowNumber = 6;
+let maxFieldNumber = 5;
 
 
 async function fetchWord(){
@@ -15,22 +17,85 @@ async function fetchWord(){
    }
 
 }
+
+function setAvailability(rownum){
+    var childNodes = document.getElementById("row"+rownum).getElementsByTagName(`*`);
+        for (let i = 0; i < childNodes.length; i++){
+            // disabling the current row's fields 
+            childNodes[i].disabled = false;
+        }
+}
   
 
-function getInput(rownum){
-    let userInput= "";
+function getInput(){
+    let isCorrect;
+    // TODO -- check to see if all fields are filled
+   
+    // iterating through rows
+   
+        // getting all the elements from the current row
+        let ustr = "";
+        var childNodes = document.getElementById("row"+rowNumber).getElementsByTagName(`*`);
+        for (let i = 0; i < childNodes.length; i++){
+            console.log(childNodes[i].value);
+            // storing value of current node
+            ustr += childNodes[i].value;
+            // disabling the current row's fields 
+            childNodes[i].disabled = true;
+        }
+        console.log(ustr);
+        checkInput(ustr)
+        if (checkInput(ustr)){
+            console.log(`success, you guess the right word!`);
+         
+        } else {
+            rowNumber+=1;
+            setAvailability(rowNumber);
+        }
+
+    
+    
+
+
+
+    /*
+    for (let i = 1; i <= maxRowNumber; i++){
+        let userInput= "";
+        for (let j = 1; j <= maxFieldNumber ; j++){
+            let letter = document.getElementById('field'+j).value;
+            userInput += letter;
+        }
+        isCorrect = checkInput(userInput);
+        console.log(`done reading, input is ${isCorrect}, with user value ${userInput}`);
+        if (isCorrect){
+            console.log(`you guessed the correct word!`);
+            break;
+        } else {
+           
+           
+            
+        }
+        
+    }
+
+
     for (let i = 1; i < 6; i++){
         let letter = document.getElementById('field'+i).value;
         userInput += letter;
-    }
-    checkInput(userInput);
+    
+    isCorrect = checkInput(userInput);
     console.log(`looking at ${userInput}`);
+    iteration++;
+  */
+   
 }
+
 
 function checkInput(str){
     // storing the letters and values of word in map
     var map = new Map();
     var inputMap = new Map();
+    var greenCount = 0;
 
     // get the number of times each letter appears
     for (let i = 0; i < word.length; i++){
@@ -48,6 +113,7 @@ function checkInput(str){
         console.log(x);
     }
 
+    console.log("\n");
     
     for (let i = 0; i < str.length; i++){
         if (!inputMap.has(str[i])){
@@ -63,11 +129,13 @@ function checkInput(str){
         console.log(x);
     }
 
+    console.log(`str length is ${str.length}`);
     // get all the green values
     for (let i = 0; i < str.length; i++){
         console.log(`looking at input ${str[i]} and word ${word[i]}`)
         if (str[i] == word[i]){
             changeColour("green", `field`+(i+1));
+            greenCount++;
         }
         else if (word.includes(str[i])){
             // if the input has more letters than necessary, leave color
@@ -80,27 +148,32 @@ function checkInput(str){
                 changeColour("rgb(255, 218, 36)", `field`+(i+1));
 
             }
-        } else {
-            // do nothing
-            
-        }
+        } 
     }
-
+    if (greenCount == 5){
+        return true;
+    } else {
+        return false;
+    }
+   
+    
 
 }
 
 
 function changeColour(colour, id){
+    var element = document.getElementById("row"+rowNumber).getElementById(id);
+    console.log(`element is ${element}`);
     document.getElementById(id).style.backgroundColor = colour;
 }
 
 
 async function main(){
     const value = await fetchWord();
-    word = value[0];
+    //word = value[0];
+    word = "hello";
 
-
-    console.log(`the word is ${value}`);
+    console.log(`the word is ${word}`);
     
 }
 
