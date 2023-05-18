c471faf647bd11accf4c36dc65d5021efe8bd70f
jvlazar <jvlazar@gmail.com>
2023-05-17 18:35:35 -0600

    flip animation works with colour synched
    -animation for flipping each field works
    -colour change of field is in synch with that of the flip

--- index.js
+++ index.js
@@ -5,76 +5,91 @@ let maxFieldNumber = 5;
 let fieldNumber = 0;
 
 // get random word
-async function fetchWord(){
+async function fetchWord() {
     // getting random word that's 5 letters long
     let url = 'https://random-word-api.vercel.app/api?words=1&length=5';
-  
+
     try {
         const response = await fetch(url);
         const value = await response.json();
         return value;
     }
-   catch (error) {
-    console.error(error);
-   }
+    catch (error) {
+        console.error(error);
+    }
 
 }
 
 // activates the shake property for the specific row, called when error message appears
-function shakeRow(rowNumber){
-    document.getElementById("row"+rowNumber).style.animation = "shake 0.5s";
-    document.getElementById("row"+rowNumber).style.animationIterationCount = 2;
-    
+function shakeRow(rowNumber) {
+    document.getElementById("row" + rowNumber).style.animation = "shake 0.5s";
+    document.getElementById("row" + rowNumber).style.animationIterationCount = 2;
+
+}
+
+function flipInputs(rowNumber, fieldNumber) {
+    console.log(`row number is ${rowNumber}`);
+    let delay = 20;
+    var childNodes = document.getElementById("row" + rowNumber).getElementsByTagName(`input`);
+    for (let i = 0; i < childNodes.length; i++) {
+        // storing value of current node
+
+        childNodes[i].style.animation = "flip 0.5s";
+        childNodes[i].style.animationDelay = delay + "ms";
+        delay += 350;
+        console.log(`at the ${i} position`);
+    }
+
+    //document.getElementById("field" + fieldNumber).style.animation = "flip 0.5s";
+    //document.getElementById("row" + rowNumber).getElementsByClassName("field" + fieldNumber).style.animationIterationCount = 2;
+
 }
 
 // timer for error message
-function messageTimer(element){
+function messageTimer(element) {
     console.log(`error message`);
-    setTimeout(function(){
-        document.getElementById(element+"_div").style.zIndex = 99;
-        document.getElementById(element+"_div").style.opacity = 1;
+    setTimeout(function () {
+        document.getElementById(element + "_div").style.zIndex = 99;
+        document.getElementById(element + "_div").style.opacity = 1;
         document.getElementById(element).innerHTML = 'Not enough letters';
         shakeRow(rowNumber);
     }, 1);
-    
-    setTimeout(function(){
-        document.getElementById(element+"_div").style.zIndex = 0;
-        document.getElementById(element+"_div").style.opacity = 0;
+
+    setTimeout(function () {
+        document.getElementById(element + "_div").style.zIndex = 0;
+        document.getElementById(element + "_div").style.opacity = 0;
         document.getElementById(element).innerHTML = '';
-        document.getElementById("row"+rowNumber).style.animation = "none";
+        document.getElementById("row" + rowNumber).style.animation = "none";
     }, 2000);
 }
 
 
 // after clicking the enter button or clicking the enter key on keyboard
-function getInput(){
-  
+function getInput() {
+
     // getting all the elements from the current row
     let ustr = "";
-    
-    var childNodes = document.getElementById("row"+rowNumber).getElementsByTagName(`input`);
-    for (let i = 0; i < childNodes.length; i++){
+
+    var childNodes = document.getElementById("row" + rowNumber).getElementsByTagName(`input`);
+    for (let i = 0; i < childNodes.length; i++) {
         // storing value of current node
         ustr += childNodes[i].value;
-        // disabling the current row's fields 
-       
-        childNodes[i].disabled = false;
     }
-         
-    ustr=ustr.toLowerCase();
+
+    ustr = ustr.toLowerCase();
     // the user hasn't filled all squares out
-    if (ustr.length != 5){
+    if (ustr.length != 5) {
         fieldNumber = 0;
         return;
     } else {
-        if (checkInput(ustr)){
-            
+        if (checkInput(ustr)) {
+
             document.getElementById("message").innerHTML = `SUCCESS! The word is "${word}"`;
-            if (rowNumber == maxRowNumber){
+            if (rowNumber == maxRowNumber) {
                 fieldNumber = 0;
             }
             return;
-        } else if (rowNumber <= maxRowNumber){
+        } else if (rowNumber <= maxRowNumber) {
             fieldNumber = 0;
         } else {
             document.getElementById(`message`).innerHTML = `You've run out of guesses. The correct word is "${word}"`;
@@ -85,16 +100,16 @@ function getInput(){
 }
 
 // compare user input to word
-function checkInput(str){
+function checkInput(str) {
     // storing the letters and values of word in map
     var map = new Map();
     var inputMap = new Map();
     var greenCount = 0;
 
     // get the number of times each letter appears
-    for (let i = 0; i < word.length; i++){
+    for (let i = 0; i < word.length; i++) {
         // if the map doens't contain the letter, add to map
-        if (!map.has(word[i])){
+        if (!map.has(word[i])) {
             map.set(word[i], 1);
         } else {
             // map has the word, update the value
@@ -102,9 +117,9 @@ function checkInput(str){
             map.set(word[i], count);
         }
     }
-   
-    for (let i = 0; i < str.length; i++){
-        if (!inputMap.has(str[i])){
+
+    for (let i = 0; i < str.length; i++) {
+        if (!inputMap.has(str[i])) {
             inputMap.set(str[i], 1);
         } else {
             // map has the word, update the value
@@ -113,66 +128,82 @@ function checkInput(str){
         }
     }
 
-    
-    for (let i = 0; i < str.length; i++){
-        if (str[i] == word[i]){
-            changeColourInput("green", (i));
-            changeColourKeyboard("green", str[i]);
-            greenCount++;
-        }
-        else if (word.includes(str[i])){
-            // if the input has more letters than necessary, leave color
-            if (Number(inputMap.get(str[i])) >  Number(map.get(str[i]))){
-                if (i >= Number(inputMap.get(str[i]))){
-                    // keep as gray
-                    changeColourInput("rgb(73, 73, 73)", i);
+    // setting the delay
+    let delay = 20;
+    var childNodes = document.getElementById("row" + rowNumber).getElementsByTagName(`input`);
+
+    // checking to see if each letter is in the correct position or not
+    for (let i = 0; i < str.length; i++) {
+        // flipping the current child node
+        childNodes[i].style.animation = "flip 0.5s";
+        childNodes[i].style.animationDelay = delay + "ms";
+
+        // change color after the delay, so the colour change matches with the flip animation
+        setTimeout(function () {
+            if (str[i] == word[i]) {
+                changeColourInput("green", (i), delay, rowNumber - 1);
+                changeColourKeyboard("green", str[i]);
+                greenCount++;
+            }
+            else if (word.includes(str[i])) {
+                // if the input has more letters than necessary, leave color
+                if (Number(inputMap.get(str[i])) > Number(map.get(str[i]))) {
+                    if (i >= Number(inputMap.get(str[i]))) {
+                        // keep as gray
+                        changeColourInput("rgb(73, 73, 73)", i, delay, rowNumber - 1);
+                    } else {
+                        // change to yellow
+                        changeColourInput("rgb(255, 218, 36)", i, delay, rowNumber - 1);
+                        changeColourKeyboard("rgb(255, 218, 36)", str[i]);
+                    }
+
                 } else {
-                    // change to yellow
-                    changeColourInput("rgb(255, 218, 36)", i);
+                    // get all the yellow values
+                    changeColourInput("rgb(255, 218, 36)", i, delay, rowNumber - 1);
                     changeColourKeyboard("rgb(255, 218, 36)", str[i]);
                 }
-               
             } else {
-                // get all the yellow values
-                changeColourInput("rgb(255, 218, 36)", i);
-                changeColourKeyboard("rgb(255, 218, 36)", str[i]);
+                // the letter does not appear in the word
+                changeColourKeyboard("rgb(110, 110, 110)", str[i]);
             }
-        } else {
-            // the letter does not appear in the word
-            changeColourKeyboard("rgb(110, 110, 110)", str[i]);
-        }
+        }, delay);
+        delay += 350;
+
     }
 
-    if (greenCount == 5){
+    if (greenCount == 5) {
         return true;
     } else {
-        rowNumber+=1;
+        rowNumber += 1;
         return false;
     }
 
 }
 
 // changes colour of the fields according to response
-function changeColourInput(colour, id){
-    var parent = document.getElementById("row"+rowNumber);
-    parent.children[id].style.backgroundColor= colour;
+function changeColourInput(colour, id, delay, rownum) {
+    var parent = document.getElementById("row" + rownum);
+    parent.children[id].style.backgroundColor = colour;
+    parent.children[id].style.transition = "background-color 0.25s ease";
+    parent.children[id].style.transition.delay = delay + "ms";
+
 }
 
 // changes colour of the keyboard buttons on display
-function changeColourKeyboard(colour, id){
+function changeColourKeyboard(colour, id) {
     document.getElementById(id).style.backgroundColor = colour;
 }
 
 // deleting the current character and moving to previous field
-function moveToPrevious(){
-    if (fieldNumber > 0){
-        document.getElementById("row"+rowNumber).getElementsByTagName(`input`)[fieldNumber-1].value = "";
+function moveToPrevious() {
+    if (fieldNumber > 0) {
+        document.getElementById("row" + rowNumber).getElementsByTagName(`input`)[fieldNumber - 1].value = "";
         // if the current field number is less than the max, focus the current field
-        if (fieldNumber < maxFieldNumber){
-            document.getElementById("row"+rowNumber).getElementsByTagName(`input`)[fieldNumber].focus();
+        if (fieldNumber < maxFieldNumber) {
+            document.getElementById("row" + rowNumber).getElementsByTagName(`input`)[fieldNumber].focus();
         } else {
             // else, focus the previous field
-            document.getElementById("row"+rowNumber).getElementsByTagName(`input`)[fieldNumber-1].focus();
+            document.getElementById("row" + rowNumber).getElementsByTagName(`input`)[fieldNumber - 1].focus();
         }
         // decrement field number
         fieldNumber -= 1;
@@ -180,28 +211,28 @@ function moveToPrevious(){
 }
 
 // getting input from the buttons
-function addInput(letter){
+function addInput(letter) {
     // if the current field number is less than the max, then display the letter
-    if (fieldNumber < maxFieldNumber){
+    if (fieldNumber < maxFieldNumber) {
         // add input to the current field and move current field to the next
-        document.getElementById("row"+rowNumber).getElementsByTagName(`input`)[fieldNumber].value = letter;
-        fieldNumber+=1;
-    } 
+        document.getElementById("row" + rowNumber).getElementsByTagName(`input`)[fieldNumber].value = letter;
+        fieldNumber += 1;
+    }
 }
 
 
 // getting input from the keyboard, called by the listener when a key is pressed 
-function addInputFromKeyboard(e){
+function addInputFromKeyboard(e) {
     // check to see if user hit the enter key and the fields are all filled up
-    if (e.which == 13 && fieldNumber == 5){
+    if (e.which == 13 && fieldNumber == 5) {
         getInput();
-    } else if (e.keyCode == 13){
+    } else if (e.keyCode == 13) {
         // if the user hit the enter key but they haven't filled the squares, error message pops up
         messageTimer(`error_message`);
-    } else if ((e.keyCode >= 65 && e.keyCode <=90) ){
+    } else if ((e.keyCode >= 65 && e.keyCode <= 90)) {
         // TODO -- only allow letters, nothing else
         // if the user hits any other key, add to input
-        addInput(String.fromCharCode(e.which)); 
+        addInput(String.fromCharCode(e.which));
     }
 }
 
@@ -209,9 +240,9 @@ function addInputFromKeyboard(e){
 
 
 
-async function main(){
+async function main() {
     // clears the fields on refresh
-    window.onload = function() {
+    window.onload = function () {
         var inputs;
         var index;
 
@@ -219,24 +250,24 @@ async function main(){
         for (var i = 0; i < inputs.length; i++) {
             // deal with inputs[index] element.
             inputs[i].value = "";
-            if (i != 0){
+            if (i != 0) {
                 inputs[i].disabled = true;
             }
         }
     }
-    
+
     // getting the word
     const value = await fetchWord();
     word = value[0];
-   
+
 
     let down = false;
     // add an event listener to the body of the page, waiting for a keydown event
-    document.getElementById(`body`).addEventListener('keydown', function(event){
+    document.getElementById(`body`).addEventListener('keydown', function (event) {
         // turn repeat off (can't press down anymore)
-        if(down) return;
+        if (down) return;
         down = true;
-        if (event.keyCode == 8){
+        if (event.keyCode == 8) {
             moveToPrevious();
         } else {
             addInputFromKeyboard(event);
@@ -246,8 +277,8 @@ async function main(){
     document.addEventListener('keyup', function () {
         down = false;
     }, false);
- 
-    
+
+
 }
 
 
